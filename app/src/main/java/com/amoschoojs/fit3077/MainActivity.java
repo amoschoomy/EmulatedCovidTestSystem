cdaa0035db9be61d837885c68e18c02e7afb9cc1
amoschoomy <acho0057@student.monash.edu>
2022-04-07 20:37:42 +0800

    Used string constant for secret key

--- app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
+++ app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
@@ -3,6 +3,7 @@ package com.amoschoojs.fit3077;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
@@ -26,34 +27,37 @@ public class MainActivity extends AppCompatActivity {
     OkHttpClient client = new OkHttpClient();
 
     // insert key here
-    String myApiKey = "<<INSERT KEY HERE>>";
+    final String MY_API_KEY = getString(R.string.api_key);
     String usersUrl = "https://fit3077.com/api/v1/user";
 
     Log.d("myTag", "here2");
-    Request request = new Request.Builder()
-            .url(usersUrl)
-            .header("Authorization", myApiKey)
-            .build();
+    Request request =
+        new Request.Builder().url(usersUrl).header("Authorization", MY_API_KEY).build();
 
     Log.d("myTag", "here3");
-    client.newCall(request).enqueue(new Callback() {
-      @Override
-      public void onFailure(Call call, IOException e) {
-        Log.d("myTag", "fail1");
-        e.printStackTrace();
-      }
+    client
+        .newCall(request)
+        .enqueue(
+            new Callback() {
+              @Override
+              public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d("myTag", "fail1");
+                e.printStackTrace();
+              }
 
-      @Override
-      public void onResponse(Call call, Response response) throws IOException {
-        if (response.isSuccessful()) {
-          String strResponse = response.body().string();
-          String strResponseCode = Integer.toString(response.code());
-          Log.d("myTag", strResponse);
-          Log.d("myTag", strResponseCode);
-        } else {
-          Log.d("myTag", "fail2");
-        }
-      }
-    });
+              @Override
+              public void onResponse(@NonNull Call call, @NonNull Response response)
+                  throws IOException {
+                if (response.isSuccessful()) {
+                  assert response.body() != null;
+                  String strResponse = response.body().string();
+                  String strResponseCode = Integer.toString(response.code());
+                  Log.d("myTag", strResponse);
+                  Log.d("myTag", strResponseCode);
+                } else {
+                  Log.d("myTag", "fail2");
+                }
+              }
+            });
   }
 }
