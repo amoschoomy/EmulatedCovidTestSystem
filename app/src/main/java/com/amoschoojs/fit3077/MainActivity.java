f1c575dcc1c80dd0e0aed307907ca33572923571
richardooi16 <rooiwork16@gmail.com>
2022-04-07 19:13:44 +0800

    Added basic login system + jwt verifier code

--- app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
+++ app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amoschoojs.fit3077;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -21,39 +22,43 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Log.d("myTag", "This is my message");
+    // This is for running my activity, can ignore this
+//    Intent switchActivityIntent = new Intent(this, LoginActivity.class);
+//    startActivity(switchActivityIntent);
 
-    OkHttpClient client = new OkHttpClient();
-
-    // insert key here
-    String myApiKey = "<<INSERT KEY HERE>>";
-    String usersUrl = "https://fit3077.com/api/v1/user";
-
-    Log.d("myTag", "here2");
-    Request request = new Request.Builder()
-            .url(usersUrl)
-            .header("Authorization", myApiKey)
-            .build();
-
-    Log.d("myTag", "here3");
-    client.newCall(request).enqueue(new Callback() {
-      @Override
-      public void onFailure(Call call, IOException e) {
-        Log.d("myTag", "fail1");
-        e.printStackTrace();
-      }
-
-      @Override
-      public void onResponse(Call call, Response response) throws IOException {
-        if (response.isSuccessful()) {
-          String strResponse = response.body().string();
-          String strResponseCode = Integer.toString(response.code());
-          Log.d("myTag", strResponse);
-          Log.d("myTag", strResponseCode);
-        } else {
-          Log.d("myTag", "fail2");
-        }
-      }
-    });
+//    Log.d("myTag", "This is my message");
+//
+//    OkHttpClient client = new OkHttpClient();
+//
+//    // insert key here
+//    String myApiKey = "<<INSERT KEY HERE>>";
+//    String usersUrl = "https://fit3077.com/api/v1/user";
+//
+//    Log.d("myTag", "here2");
+//    Request request = new Request.Builder()
+//            .url(usersUrl)
+//            .header("Authorization", myApiKey)
+//            .build();
+//
+//    Log.d("myTag", "here3");
+//    client.newCall(request).enqueue(new Callback() {
+//      @Override
+//      public void onFailure(Call call, IOException e) {
+//        Log.d("myTag", "fail1");
+//        e.printStackTrace();
+//      }
+//
+//      @Override
+//      public void onResponse(Call call, Response response) throws IOException {
+//        if (response.isSuccessful()) {
+//          String strResponse = response.body().string();
+//          String strResponseCode = Integer.toString(response.code());
+//          Log.d("myTag", strResponse);
+//          Log.d("myTag", strResponseCode);
+//        } else {
+//          Log.d("myTag", "fail2");
+//        }
+//      }
+//    });
   }
 }
