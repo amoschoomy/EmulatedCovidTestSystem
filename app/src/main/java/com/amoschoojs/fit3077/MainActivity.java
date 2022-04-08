8ff5c28e06cee2f7f1cb88d954b1c7ce22de4eb1
amoschoomy <acho0057@student.monash.edu>
2022-04-08 23:47:52 +0800

    added UI for search testing site

--- app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
+++ app/src/main/java/com/amoschoojs/fit3077/MainActivity.java
@@ -1,19 +1,9 @@
 package com.amoschoojs.fit3077;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,42 +12,49 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Log.d("myTag", "This is my message");
+    Intent switchActivityIntent = new Intent(this, SearchTestingSite.class);
+    startActivity(switchActivityIntent);
 
-    OkHttpClient client = new OkHttpClient();
+    //    Intent switchActivityIntent = new Intent(this, LoginActivity.class);
+    //    startActivity(switchActivityIntent);
 
-    // insert key here
-    final String MY_API_KEY = getString(R.string.api_key);
-    String usersUrl = "https://fit3077.com/api/v1/user";
-
-    Log.d("myTag", "here2");
-    Request request =
-        new Request.Builder().url(usersUrl).header("Authorization", MY_API_KEY).build();
-
-    Log.d("myTag", "here3");
-    client
-        .newCall(request)
-        .enqueue(
-            new Callback() {
-              @Override
-              public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                Log.d("myTag", "fail1");
-                e.printStackTrace();
-              }
-
-              @Override
-              public void onResponse(@NonNull Call call, @NonNull Response response)
-                  throws IOException {
-                if (response.isSuccessful()) {
-                  assert response.body() != null;
-                  String strResponse = response.body().string();
-                  String strResponseCode = Integer.toString(response.code());
-                  Log.d("myTag", strResponse);
-                  Log.d("myTag", strResponseCode);
-                } else {
-                  Log.d("myTag", "fail2");
-                }
-              }
-            });
+    //    Log.d("myTag", "This is my message");
+    //
+    //    OkHttpClient client = new OkHttpClient();
+    //
+    //    // insert key here
+    //    final String MY_API_KEY = getString(R.string.api_key);
+    //    String usersUrl = "https://fit3077.com/api/v1/user";
+    //
+    //    Log.d("myTag", "here2");
+    //    Request request =
+    //        new Request.Builder().url(usersUrl).header("Authorization", MY_API_KEY).build();
+    //
+    //    Log.d("myTag", "here3");
+    //    client
+    //        .newCall(request)
+    //        .enqueue(
+    //            new Callback() {
+    //              @Override
+    //              public void onFailure(@NonNull Call call, @NonNull IOException e) {
+    //                Log.d("myTag", "fail1");
+    //                e.printStackTrace();
+    //              }
+    //
+    //              @Override
+    //              public void onResponse(@NonNull Call call, @NonNull Response response)
+    //                  throws IOException {
+    //                if (response.isSuccessful()) {
+    //                  assert response.body() != null;
+    //                  String strResponse = response.body().string();
+    //                  String strResponseCode = Integer.toString(response.code());
+    //                  Log.d("myTag", strResponse);
+    //                  Log.d("myTag", strResponseCode);
+    //                } else {
+    //                  Log.d("myTag", "fail2");
+    //                }
+    //              }
+    //            });
+    //  }
   }
 }
