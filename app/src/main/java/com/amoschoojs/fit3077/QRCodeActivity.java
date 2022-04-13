431bba4f3d421342516a91ecc94ee3523f80a2db
richardooi16 <rooiwork16@gmail.com>
2022-04-13 17:47:03 +0800

    Added QR Code activity and generator

---
+++ app/src/main/java/com/amoschoojs/fit3077/QRCodeActivity.java
@@ -0,0 +1,67 @@
+package com.amoschoojs.fit3077;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    ImageView QRCodeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+
+        QRCodeView = findViewById(R.id.QRCodeView);
+        String infoText = "test";
+
+        try {
+            Bitmap QRCode = generateQRCode(infoText);
+            QRCodeView.setImageBitmap(QRCode);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Generates a QRCode (Bitmap) that returns the infoText when scanned.
+     */
+    private Bitmap generateQRCode(String infoText) throws WriterException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        int length = 250;
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(infoText, BarcodeFormat.QR_CODE, length, length);
+        int[] pixels = new int[length * length];
+
+        int xlength, ylength;
+        xlength = ylength = length;
+
+        for (int y = 0; y < ylength; y++) {
+            int offset = y * xlength;
+            for (int x = 0; x < xlength; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap myBitmap = Bitmap.createBitmap(length, length, Bitmap.Config.ARGB_8888);
+        myBitmap.setPixels(pixels, 0, length, 0, 0, length, length);
+
+        return myBitmap;
+
+    }
+}
