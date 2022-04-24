ddd58bfb31911d88f0c03be9cf2ff63c3c483081
amoschoomy <acho0057@student.monash.edu>
2022-04-24 20:28:23 +0800

    Change Booking status when user submit form

--- app/src/main/java/com/amoschoojs/fit3077/OnSiteTestingActivity.java
+++ app/src/main/java/com/amoschoojs/fit3077/OnSiteTestingActivity.java
@@ -17,22 +17,28 @@ import okhttp3.RequestBody;
 
 public class OnSiteTestingActivity extends AppCompatActivity {
 
-    Boolean feverStatus, coughStatus, throatStatus, fatigueStatus, diarrheaStatus, nauseaStatus, jointStatus;
+  Boolean feverStatus,
+      coughStatus,
+      throatStatus,
+      fatigueStatus,
+      diarrheaStatus,
+      nauseaStatus,
+      jointStatus;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_on_site_testing);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_on_site_testing);
 
-        RadioGroup feverRadioGroup = findViewById(R.id.FeverRadioGroup);
-        RadioGroup coughRadioGroup = findViewById(R.id.CoughRadioGroup);
-        RadioGroup throatRadioGroup = findViewById(R.id.ThroatRadioGroup);
-        RadioGroup fatigueRadioGroup = findViewById(R.id.FatigueRadioGroup);
-        RadioGroup diarrheaRadioGroup = findViewById(R.id.DiarrheaRadioGroup);
-        RadioGroup nauseaRadioGroup = findViewById(R.id.NauseaRadioGroup);
-        RadioGroup jointRadioGroup = findViewById(R.id.JointRadioGroup);
+    RadioGroup feverRadioGroup = findViewById(R.id.FeverRadioGroup);
+    RadioGroup coughRadioGroup = findViewById(R.id.CoughRadioGroup);
+    RadioGroup throatRadioGroup = findViewById(R.id.ThroatRadioGroup);
+    RadioGroup fatigueRadioGroup = findViewById(R.id.FatigueRadioGroup);
+    RadioGroup diarrheaRadioGroup = findViewById(R.id.DiarrheaRadioGroup);
+    RadioGroup nauseaRadioGroup = findViewById(R.id.NauseaRadioGroup);
+    RadioGroup jointRadioGroup = findViewById(R.id.JointRadioGroup);
     EditText bookingPin = findViewById(R.id.pinInput);
-        Button recButton = findViewById(R.id.RecButton);
+    Button recButton = findViewById(R.id.RecButton);
     recButton.setOnClickListener(
         new View.OnClickListener() {
           @Override
@@ -72,7 +78,8 @@ public class OnSiteTestingActivity extends AppCompatActivity {
               String bookingID = array[0];
               String jsonstr =
                   String.format(
-                      "{\"additionalInfo\":{\"recommendedTest\":\"%s\"}}", recommendedTest);
+                      "{\"additionalInfo\":{\"recommendedTest\":\"%s\"}, \"status\": \"PROCESSED\"}",
+                      recommendedTest);
 
               RequestBody body =
                   RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonstr);
@@ -85,18 +92,19 @@ public class OnSiteTestingActivity extends AppCompatActivity {
             }
           }
         });
-    }
+  }
 
-    /**
-     * Checks If Yes or No is selected from the Radio Button Group. If yes is selected, return true,
-     * otherwise return false.
-     * @throws NullPointerException Nothing is selected in the radio group.
-     */
-    private boolean checkStatus(RadioGroup v) throws NullPointerException {
-        int radioId = v.getCheckedRadioButtonId();
+  /**
+   * Checks If Yes or No is selected from the Radio Button Group. If yes is selected, return true,
+   * otherwise return false.
+   *
+   * @throws NullPointerException Nothing is selected in the radio group.
+   */
+  private boolean checkStatus(RadioGroup v) throws NullPointerException {
+    int radioId = v.getCheckedRadioButtonId();
 
-        RadioButton buttonSelected = findViewById(radioId);
-        String choice = buttonSelected.getText().toString();
-        return choice.equalsIgnoreCase("Yes");
-    }
+    RadioButton buttonSelected = findViewById(radioId);
+    String choice = buttonSelected.getText().toString();
+    return choice.equalsIgnoreCase("Yes");
+  }
 }
